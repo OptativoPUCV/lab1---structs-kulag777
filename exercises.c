1006ebc63f211191ddad94bc8802b0bf3c560bd0
Your Name <you@example.com>
2024-03-14 21:48:59 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -67,25 +67,24 @@ ordenados y sus tamaños, y luego fusione estos dos
 arreglos en un tercer arreglo también ordenado.
 */
 void mergeSortedArrays(int arr1[], int size1, int arr2[], int size2,
-                       int result[]) {
-  int* nuevoArreglo = (int*) malloc((size1 + size2) * sizeof(int));
-  if(nuevoArreglo == NULL) return EXIT_FAILURE;
+                     int result[]) {
+int* nuevoArreglo = (int*) malloc((size1 + size2) * sizeof(int));
+if(nuevoArreglo == NULL) return EXIT_FAILURE;
 
-  int x = 0;
-  int y = 0;
+int x = 0;
+int y = 0;
 
-  for(int i = 0; i < (size1); i++)){
-      if(arr1[y] <= arr2[x])){
-        nuevoArreglo[i] = arr1[y];
-        y++;
-      }
-      else{
-        nuevoArreglo[i] = arr2[x];
-        x++;
-      }
+for(int i = 0; i < (size1); i++)){
+    if(arr1[y] <= arr2[x])){
+      nuevoArreglo[i] = arr1[y];
+      y++;
     }
-  return nuevoArreglo;
-                       }
+    else{
+      nuevoArreglo[i] = arr2[x];
+      x++;
+    }
+  }
+}
 
 /*
 Ejercicio 5: Comprobación de Ordenación
