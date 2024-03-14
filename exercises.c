9ac4f82647fb7c97c716cfb6a67d8377553291ac
Your Name <you@example.com>
2024-03-14 22:23:20 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -73,7 +73,7 @@ void mergeSortedArrays(int arr1[], int size1, int arr2[], int size2, int result[
 
   for(int i = 0; i < (size1 + size2); i++)
   {
-    if (arr1[x] <= arr2[y] && x < size1))
+    if (arr1[x] <= arr2[y] && x <= size1))
     {
       result[i] = arr1[x];
       x++;
