7b7cc1afb6a6ed4f48f2b84f495a2f1b9913ff64
Your Name <you@example.com>
2024-03-16 01:10:23 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -137,7 +137,10 @@ typedef struct {
 
 void inicializarLibro(Libro *libro, const char *titulo, const char *nombreAutor,
                       int anioNacimiento, int anioPublicacion) {
-  
+  strcpy(libro->titulo, titulo);
+  strcpy(libro->autor.nombre, nombreAutor);
+  libro->autor.anioNacimiento = anioNacimiento;
+  libro->anioPublicacion = anioPublicacion;
                       }
 
 /*
